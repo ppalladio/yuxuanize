f36bf291453add7a4793e89786475850ea9cbf8b
Yuxuan <nine.slide@gmail.com>
2024-04-06 12:35:07 +0200

    add eng cv v1

--- app/(cv)/cv/[lang]/page.tsx
+++ app/(cv)/cv/[lang]/page.tsx
@@ -2,9 +2,16 @@
 import { useParams, useSearchParams } from 'next/navigation';
 import CvEn from '../CvEn';
 import CvEs from '../CvEs';
+import { useEffect, useState } from 'react';
 
 const CvPage = () => {
-    const params = useParams();
+	const params = useParams();
+
+    const [isMounted, setIsMounted] = useState(false);
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+    if (!isMounted) return null;
     console.log(params);
     if (params.lang === 'en') {
         return <CvEn />;
